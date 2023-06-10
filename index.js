c9674cc93cb7bb8d8794560d27dae20a049c740c
Ilmari Kurkela <ilmari.kurkela@esedulainen.fi>
2023-06-10 13:13:21 +0300

    3.1-3.6

--- index.js
+++ index.js
@@ -1,28 +1,125 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
+app.use(express.json())
 
-let notes = [
+let persons = [
   {
-    id: 1,
-    content: "HTML is easy",
-    important: true
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
   },
   {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
   },
   {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "test",
+    "number": "1",
+    "id": 5
+  },
+  {
+    "name": "tessss",
+    "number": "1",
+    "id": 6
+  },
+  {
+    "name": "ss",
+    "number": "d",
+    "id": 7
+  },
+  {
+    "name": "ss d",
+    "number": "",
+    "id": 8
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  )
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  } else if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  } else if (persons.map(p => {return p.name}).includes(body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: persons.length + 1,
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
